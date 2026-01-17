f4bd63b99c83cc1f7c5c35f117bd3902f5017a4d
hxl <hxl@git.com>
2026-01-17 15:12:23 +0800

    [2026/1/17]scss 文件自动导入

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,9 +5,17 @@
 <template>
   <div>
     登录页
+    <div class="scss-test">
+      测试var.scss自动导入
+    </div>
   </div>
 </template>
 
-<style scoped>
-
+<!-- 需要明确指定 lang="scss" 才能使用 scss 语法 -->
+<style scoped lang="scss">
+  .scss-test {
+    width: 100px;
+    height: 100px;
+    background-color: $priceColor;
+  }
 </style>
